3d1ea508696d38348171aceb7ff2e8f1488f2843
Atin Agnihotri <atin.agnihotri@paytmmoney.com>
2021-06-08 14:31:54 +0530

    Initial Commit

---
+++ animations-in-swiftui/animations_in_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  animations_in_swiftuiApp.swift
+//  animations-in-swiftui
+//
+//  Created by Atin Agnihotri on 08/06/21.
+//
+
+import SwiftUI
+
+@main
+struct animations_in_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
